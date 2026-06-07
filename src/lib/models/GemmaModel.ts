31771cd4ab057473139648489daa54894228b61d
Jaid <6216144+Jaid@users.noreply.github.com>
2026-06-07 04:21:59 +0100

    Improved SVGs
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/lib/models/GemmaModel.ts
+++ src/lib/models/GemmaModel.ts
@@ -4,6 +4,6 @@ export default class GemmaModel extends Model {
   override name = 'Gemma'
   override subname = '4 31B it'
   override get icon() {
-    return '/google.svg'
+    return '/gemini.svg'
   }
 }
