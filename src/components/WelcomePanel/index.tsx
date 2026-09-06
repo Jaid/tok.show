ede4eff04471453da280121975ed9235f60779cb
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-06 05:50:44 +0100

    Added cog animation
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/components/WelcomePanel/index.tsx
+++ src/components/WelcomePanel/index.tsx
@@ -1,13 +1,30 @@
 import type {FunctionComponent} from 'react'
 
+import {useId} from 'react'
+
+
 import AppTitle from '#component/AppTitle'
 import ExampleList from '#component/ExampleList'
 import Tok from '#component/Tok'
 
+import Cog from './cog.svg?react'
 import css from './style.module.sass'
 
 const WelcomePanel: FunctionComponent = () => {
+  const patternId = useId()
   return <div className={css.container}>
+    {[0, 1].map(layer => <svg key={layer} className={css.cogs} aria-hidden='true'>
+      <defs>
+        <pattern id={`${patternId}-${layer}`} width='160' height='160' x={layer * 80} y={layer * 80} patternUnits='userSpaceOnUse'>
+          <g transform='translate(80 80)'>
+            <g className={css.cog}>
+              <Cog x={-80} y={-80} width={160} height={160} />
+            </g>
+          </g>
+        </pattern>
+      </defs>
+      <rect width='100%' height='100%' fill={`url(#${patternId}-${layer})`} />
+    </svg>)}
     <div className={css.tokRow}>
       <Tok />
     </div>
